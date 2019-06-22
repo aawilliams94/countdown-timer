94cf6e0eecc0307420dfec2b946b94063e5e6197
Ashanti Williams <ashanti.a.williams@hotmail.com>
2019-06-22 15:13:28 -0600

    Iteration 0 of timer

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var minute: String = ""
+    var second:String = ""
+    
+    @IBOutlet var secondsInputField: UITextField!
+    @IBOutlet var minutesInputField: UITextField!
+    
+    @IBOutlet var minuteLabel: UILabel!
+    @IBOutlet var secondLabel: UILabel!
+    
+    @IBAction func setTimerButton(_ sender: UIButton) {
+    minute = minutesInputField.text!
+    second = secondsInputField.text!
+        
+        minuteLabel.text = "\(minute)"
+        secondLabel.text = "\(second)"
+        
+    }
+    //Countdown Stuff
+    
+    var timer:Timer?
+    var timeLeft = 0
+    
+    timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
