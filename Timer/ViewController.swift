037b33acfb216d35ae646c4ce0d6a433fcd596fb
Ashanti Williams <ashanti.a.williams@hotmail.com>
2019-06-22 14:20:56 -0600

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Ashanti Williams on 6/22/19.
+//  Copyright © 2019 Ashanti Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
